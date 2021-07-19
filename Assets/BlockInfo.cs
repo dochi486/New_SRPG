a20e11369fb78457ed186f3e63b2d14a1f07645e
dochi486 <93mskim@gmail.com>
2021-07-19 19:21:04 +0900

    터치로 플레이어 이동, 플레이어가 이동방향 바라보도록 구현
    OnMouseDown, OnTouch, LookAt

--- Assets/BlockInfo.cs
+++ Assets/BlockInfo.cs
@@ -11,4 +11,10 @@ public enum BlockType
 public class BlockInfo : MonoBehaviour
 {
     public BlockType blockType;
+
+    private void OnMouseDown()
+    {
+        //GroundManager를 싱글턴으로 만들어서 마우스 다운되면.. 이동하게!
+        GroundManager.Instance.OnTouch(transform.position);
+    }
 }
--- Assets/GroundManager.cs
+++ Assets/GroundManager.cs
@@ -1,25 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GroundManager : MonoBehaviour
+public class GroundManager : SingletonMonoBehavior<GroundManager>
 {
     public Dictionary<Vector2Int, int> map = new Dictionary<Vector2Int, int>(); //맵의 좌표로 정보 접근
     public List<int> passableValues = new List<int>(); //갈 수 있는 지역(밟을 수 있는 타일)
     public Vector2Int goalPos; //목표지점
+
+    internal void OnTouch(Vector3 position)
+    {
+
+        Vector2Int findPos = new Vector2Int((int)position.x, (int)position.z);
+        FindPath(findPos);
+    }
+
     public Vector2Int playerPos; //시작지점
 
     public Transform player;
-    public Transform goal;
+    //public Transform goal;
 
 
-    [ContextMenu("길 찾기 테스트")] //컨텍스트 메뉴로도 코루틴 작동하는지?
-    void Start()
+    //[ContextMenu("길 찾기 테스트")] //컨텍스트 메뉴로도 코루틴 작동하는지?
+    void FindPath(Vector2Int goalPos)
     {
-        StartCoroutine(FindPath());
+        StopAllCoroutines();
+        StartCoroutine(FindPathCo(goalPos));
     }
 
-    IEnumerator FindPath()
+    IEnumerator FindPathCo(Vector2Int goalPos)
     {
         passableValues = new List<int>();
         passableValues.Add((int)BlockType.Walkable);
@@ -37,8 +47,8 @@ public class GroundManager : MonoBehaviour
         playerPos.x = (int)player.position.x;
         playerPos.y = (int)player.position.z; //벡터2를 쓰고 있지만 실제로 y말고 z축의 값을 사용하고 있기 때문에 암시적 형변환+z사용
 
-        goalPos.x = (int)goal.position.x;
-        goalPos.y = (int)goal.position.z;
+        //goalPos.x = (int)goal.position.x;
+        //goalPos.y = (int)goal.position.z;
 
         var path = PathFinding2D.find4(playerPos, goalPos, map, passableValues);
         if (path.Count == 0)
@@ -48,6 +58,7 @@ public class GroundManager : MonoBehaviour
             foreach (var item in path)
             {
                 Vector3 playerNewPos = new Vector3(item.x, 0, item.y);
+                player.LookAt(playerNewPos);
                 player.position = playerNewPos;
                 yield return new WaitForSeconds(0.5f);
             }
