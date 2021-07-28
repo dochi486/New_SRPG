0e0482d0179f9d0fb47a0b4ef49676be35e1589a
dochi486 <93mskim@gmail.com>
2021-07-28 20:06:10 +0900

    아이템 추가

--- Assets/GlobalData.cs
+++ Assets/GlobalData.cs
@@ -13,14 +13,31 @@ public class PlayerLevelData
     public int maxExp;
 }
 
+
+[System.Serializable]
+public class ItemData
+{
+    //아이템의 착용 가능레벨, 판매 가격, 구입 가격, 아이콘 이름을 담는 클래스
+    public int ID;
+    public int equipLevel;
+    public int sellPrice;
+    public int buyPrice;
+    public string iconName;
+}
+
+
 public class GlobalData : SingletonMonoBehavior<GlobalData>
 {
     [SerializeField]public List<PlayerLevelData> playerDatas;
     public Dictionary<int, PlayerLevelData> playerDataMap;
 
+    [SerializeField] public List<ItemData> itemDatas;
+    public Dictionary<int, ItemData> itemDataMap;
+
     protected override void OnInit()
     {
-        playerDataMap = playerDatas.ToDictionary(x=> x.level);
+        playerDataMap = playerDatas.ToDictionary(x=> x.level); //레벨을 기준으로 리스트와 맵 초기화
+        itemDataMap = itemDatas.ToDictionary(x => x.ID); //아이템 ID기준으로 리스트, 맵 초기화
     }
 }
 
