11cdffb9f9d60e2600c7611b5ce3d1a9f5f0d42a
dochi486 <93mskim@gmail.com>
2021-07-28 20:10:31 +0900

    드랍되는 아이템 정보, 모든 아이템 정보를 담는 딕셔너리맵, 리스트 생성 및 초기화

--- Assets/GlobalData.cs
+++ Assets/GlobalData.cs
@@ -25,6 +25,20 @@ public class ItemData
     public string iconName;
 }
 
+[System.Serializable]
+public class DropItemGroupData //드랍되는 아이템의 정보들
+{
+    //아이템의 착용 가능레벨, 판매 가격, 구입 가격, 아이콘 이름을 담는 클래스
+    public int ID;
+    public List<ItemDropInfo> dropItems; //아이템의 드랍확률 같은 정보를 담고있는 리스트
+}
+
+[System.Serializable]
+public class ItemDropInfo
+{
+    public int dropItemID; //드랍되는 아이템의 id
+    public float ratio; //아이템 드랍률
+}
 
 public class GlobalData : SingletonMonoBehavior<GlobalData>
 {
@@ -34,10 +48,14 @@ public class GlobalData : SingletonMonoBehavior<GlobalData>
     [SerializeField] public List<ItemData> itemDatas;
     public Dictionary<int, ItemData> itemDataMap;
 
+    [SerializeField] public List<DropItemGroupData> dropItemGroupDatas;
+    public Dictionary<int, DropItemGroupData> dropItemGroupDataMap;
+
     protected override void OnInit()
     {
         playerDataMap = playerDatas.ToDictionary(x=> x.level); //레벨을 기준으로 리스트와 맵 초기화
         itemDataMap = itemDatas.ToDictionary(x => x.ID); //아이템 ID기준으로 리스트, 맵 초기화
+        dropItemGroupDataMap = dropItemGroupDatas.ToDictionary(x => x.ID); //드랍아이템 ID를 가지고 맵을 초기화
     }
 }
 
